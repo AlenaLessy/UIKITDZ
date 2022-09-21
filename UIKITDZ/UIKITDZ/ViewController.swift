9fc6b89732761e9b37d9da0fca8b9b904aeb5ef0
Алена Карпова <alenapancenko00@gmail.com>
2022-09-21 17:22:35 +0300

    init

---
+++ UIKITDZ/UIKITDZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKITDZ
+//
+//  Created by Алена Панченко on 21.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
