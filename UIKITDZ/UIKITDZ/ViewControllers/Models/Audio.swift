992c279a0efa8d930dc8709a69d6aaa7e9d4d7a4
Алена Карпова <alenapancenko00@gmail.com>
2022-09-25 10:44:13 +0300

    Сделала 6 задание - UISlider Аудиоплеер

---
+++ UIKITDZ/UIKITDZ/ViewControllers/Models/Audio.swift
@@ -0,0 +1,17 @@
+//
+//  Audio.swift
+//  UIKITDZ
+//
+//  Created by Алена Панченко on 24.09.2022.
+//
+
+import Foundation
+
+import AVFoundation
+
+/// Список аудио композиций
+ struct Song {
+    let name: String
+    let imageName: String
+    let duration: String
+}
