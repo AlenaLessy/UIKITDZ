4d73da7eb5c57854dec57a8c697bd0562850bd8e
Алена Карпова <alenapancenko00@gmail.com>
2022-09-26 09:13:25 +0300

    Сделала 5 задание - Пикеры

---
+++ UIKITDZ/UIKITDZ/BirthDayViewController.swift
@@ -0,0 +1,183 @@
+//
+//  BirthDayViewController.swift
+//  UIKITDZ
+//
+//  Created by Алена Панченко on 25.09.2022.
+//
+
+import UIKit
+
+final class BirthDayViewController: UIViewController {
+
+    // people Jeremy
+    private lazy var namePeopleOneLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Jeremy"
+        label.textColor = #colorLiteral(red: 0.1175499335, green: 0.1175499335, blue: 0.1175499335, alpha: 1)
+        label.frame = CGRect(x: 80, y: 145, width: 80, height: 40)
+        label.font = UIFont(name: "Arial Bold", size: 17)
+        return label
+    }()
+    
+    private lazy var peopleOneImage: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(systemName: "person.circle.fill")
+        imageView.tintColor = .gray
+        imageView.frame = CGRect(x: 30, y: 150, width: 50, height: 50)
+        return imageView
+    }()
+    
+    private lazy var howManyYearsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "10 марта, в среду исполнится 25 лет"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.frame = CGRect(x: 80, y: 163, width: 270, height: 40)
+        label.font = UIFont(name: "Arial", size: 14)
+        return label
+    }()
+    
+    private lazy var daysBeforeBirthdayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "18 дней"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.frame = CGRect(x: 300, y: 145, width: 250, height: 40)
+        label.font = UIFont(name: "Arial", size: 17)
+        return label
+    }()
+    
+    private lazy var grayStripe: UIView = {
+        var line = UIView()
+        line.backgroundColor = .gray
+        line.frame = CGRect(x: 10, y: 210, width: 500, height: 0.7)
+        return line
+    }()
+    
+    // people Maria Lui
+    
+    private lazy var namePeopleTwoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Maria Lui"
+        label.textColor = #colorLiteral(red: 0.1175499335, green: 0.1175499335, blue: 0.1175499335, alpha: 1)
+        label.frame = CGRect(x: 80, y: 245, width: 80, height: 40)
+        label.font = UIFont(name: "Arial Bold", size: 17)
+        return label
+    }()
+    
+    private lazy var peopleTwoImage: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(systemName: "person.circle.fill")
+        imageView.tintColor = .gray
+        imageView.frame = CGRect(x: 30, y: 250, width: 50, height: 50)
+        return imageView
+    }()
+    
+    private lazy var howManyYearsPeopleTwoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "30 марта, в четверг исполнится 20 лет"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.frame = CGRect(x: 80, y: 263, width: 270, height: 40)
+        label.font = UIFont(name: "Arial", size: 14)
+        return label
+    }()
+    
+    private lazy var daysBeforeBirthdayPeopleTwoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "28 дней"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.frame = CGRect(x: 300, y: 245, width: 250, height: 40)
+        label.font = UIFont(name: "Arial", size: 17)
+        return label
+    }()
+    
+    private lazy var grayStripeTwo: UIView = {
+        var line = UIView()
+        line.backgroundColor = .gray
+        line.frame = CGRect(x: 10, y: 310, width: 500, height: 0.7)
+        return line
+    }()
+    
+    // People Jony Stark
+    
+    private lazy var namePeopleThreeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Jony Stark"
+        label.textColor = #colorLiteral(red: 0.1175499335, green: 0.1175499335, blue: 0.1175499335, alpha: 1)
+        label.frame = CGRect(x: 80, y: 345, width: 150, height: 40)
+        label.font = UIFont(name: "Arial Bold", size: 17)
+        return label
+    }()
+    
+    private lazy var peopleThreeImage: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.image = UIImage(systemName: "person.circle.fill")
+        imageView.tintColor = .gray
+        imageView.frame = CGRect(x: 30, y: 350, width: 50, height: 50)
+        return imageView
+    }()
+    
+    private lazy var howManyYearsPeopleThreeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "20 апреля, в субботу исполнится 25 лет"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.frame = CGRect(x: 80, y: 363, width: 270, height: 40)
+        label.font = UIFont(name: "Arial", size: 14)
+        return label
+    }()
+    
+    private lazy var daysBeforeBirthdayPeopleThreeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "53 дней"
+        label.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        label.frame = CGRect(x: 300, y: 345, width: 250, height: 40)
+        label.font = UIFont(name: "Arial", size: 17)
+        return label
+    }()
+    
+    private lazy var grayStripeThree: UIView = {
+        var line = UIView()
+        line.backgroundColor = .gray
+        line.frame = CGRect(x: 10, y: 410, width: 500, height: 0.7)
+        return line
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubView()
+        configureView()
+    }
+    
+    private func configureSubView() {
+        view.backgroundColor = .white
+        navigationItem.title = "Birthday"
+        view.addSubview(namePeopleOneLabel)
+        view.addSubview(peopleOneImage)
+        view.addSubview(howManyYearsLabel)
+        view.addSubview(grayStripe)
+        view.addSubview(daysBeforeBirthdayLabel)
+        view.addSubview(namePeopleTwoLabel)
+        view.addSubview(daysBeforeBirthdayPeopleTwoLabel)
+        view.addSubview(grayStripeTwo)
+        view.addSubview(howManyYearsPeopleTwoLabel)
+        view.addSubview(peopleTwoImage)
+        view.addSubview(namePeopleThreeLabel)
+        view.addSubview(daysBeforeBirthdayPeopleThreeLabel)
+        view.addSubview(grayStripeThree)
+        view.addSubview(howManyYearsPeopleThreeLabel)
+        view.addSubview(peopleThreeImage)
+    }
+    private func configureView() {
+        view.backgroundColor = .white
+        navigationItem.title = "Birthday"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                                 target: self,
+                                                                 action: #selector(addPersonAction))
+    }
+    @objc private func addPersonAction() {
+        let details = AddPersonViewController()
+        details.modalTransitionStyle = .flipHorizontal
+        self.present(details, animated: true)
+    }
+}
---
+++ UIKITDZ/UIKITDZ/RegistrationViewController.swift
@@ -0,0 +1,149 @@
+//
+//  ViewController.swift
+//  UIKITDZ
+//
+//  Created by Алена Панченко on 21.09.2022.
+//
+
+import UIKit
+
+extension UITextField {
+    func setBottomBorder(color: UIColor) {
+             borderStyle = .none
+             layer.backgroundColor = UIColor.white.cgColor
+             layer.masksToBounds = false
+             layer.shadowColor = color.cgColor
+             layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+             layer.shadowOpacity = 1.0
+             layer.shadowRadius = 0.0
+         }
+}
+
+/// Экран регистрации пользователей
+class RegistrationViewController: UIViewController {
+
+    private lazy var loginButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Войти", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.backgroundColor = #colorLiteral(red: 0.4902775884, green: 0.6610349417, blue: 0.9130681157, alpha: 1)
+        btn.frame = CGRect(x: 60, y: 570, width: 290, height: 40)
+        btn.layer.cornerRadius = 7
+        btn.addTarget(self, action: #selector(loginButtonAction), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var signInLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sign In"
+        label.textColor = #colorLiteral(red: 0.1175499335, green: 0.1175499335, blue: 0.1175499335, alpha: 1)
+        label.frame = CGRect(x: 60, y: 250, width: 80, height: 40)
+        label.font = UIFont(name: "Arial Bold", size: 22)
+        return label
+    }()
+    
+    private lazy var eMailLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Email"
+        label.textColor = #colorLiteral(red: 0.4902775884, green: 0.6610349417, blue: 0.9130681157, alpha: 1)
+        label.frame = CGRect(x: 60, y: 300, width: 50, height: 30)
+        label.font = UIFont(name: "Arial Bold", size: 15)
+        return label
+    }()
+    
+    private lazy var passwordLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Password"
+        label.textColor = #colorLiteral(red: 0.4902775884, green: 0.6610349417, blue: 0.9130681157, alpha: 1)
+        label.frame = CGRect(x: 60, y: 400, width: 80, height: 30)
+        label.font = UIFont(name: "Arial Bold", size: 15)
+        return label
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Введите ваш eMail"
+        textField.frame = CGRect(x: 60, y: 320, width: 270, height: 50)
+        textField.setBottomBorder(color: .systemGray5)
+        return textField
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Введите ваш eMail"
+        textField.frame = CGRect(x: 60, y: 420, width: 270, height: 50)
+        textField.setBottomBorder(color: .systemGray5)
+        return textField
+    }()
+    
+    private lazy var faceIDLoginLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Вход по Face ID"
+        label.textColor = #colorLiteral(red: 0.1997019649, green: 0.1997019649, blue: 0.1997019649, alpha: 1)
+        label.frame = CGRect(x: 190, y: 500, width: 100, height: 30)
+        label.font = UIFont(name: "Arial Bold", size: 12)
+        return label
+    }()
+    
+    private lazy var faceIdSwith: UISwitch = {
+        var switcher = UISwitch()
+        switcher.isOn = true
+        switcher.frame = CGRect(x: 300, y: 500, width: 30, height: 30)
+        switcher.tintColor = .gray
+      //  switcher.addTarget(self, action: #selector(switchFaceId), for: .touchDown)
+        return switcher
+    }()
+    
+    private lazy var birthdayReminderLabel: UILabel = {
+        var label = UILabel()
+        label.text = "Birthday Reminder"
+        label.textColor = #colorLiteral(red: 0.4902775884, green: 0.6610349417, blue: 0.9130681157, alpha: 1)
+        label.layer.borderWidth = CGFloat(1)
+        label.textAlignment = .center
+        label.frame = CGRect(x: 0, y: 100, width: 200, height: 70)
+        label.center.x = view.center.x
+        return label
+    }()
+    
+    private lazy var hideTextButtonAction: UIButton = {
+        var button = UIButton()
+        button.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
+        button.frame = CGRect(x: 300, y: 435, width: 30, height: 30)
+        button.tintColor = .gray
+       button.addTarget(self, action: #selector(hideTextButtonToggle), for: .touchDown)
+        return button
+    }()
+    
+    @objc private func hideTextButtonToggle() {
+        if self.passwordTextField.isSecureTextEntry {
+            self.passwordTextField.isSecureTextEntry = false
+            self.hideTextButtonAction.setImage(UIImage(systemName: "eye.fill"), for: .normal)
+        } else {
+            self.passwordTextField.isSecureTextEntry = true
+            self.hideTextButtonAction.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
+        }
+    }
+    
+    @objc private func loginButtonAction() {
+        let birthdayVC = BirthDayViewController()
+        self.navigationController?.pushViewController(birthdayVC, animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSubView()
+    }
+    private func configureSubView() {
+        view.addSubview(loginButton)
+        view.addSubview(signInLabel)
+        view.addSubview(eMailLabel)
+        view.addSubview(passwordLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(faceIDLoginLabel)
+        view.addSubview(faceIdSwith)
+        view.addSubview(birthdayReminderLabel)
+        view.addSubview(hideTextButtonAction)
+        view.backgroundColor = .white
+    }
+}
