cceb9d3cd4df8c61eb0c8d7e2d067ce4619648fb
Алена Карпова <alenapancenko00@gmail.com>
2022-10-07 10:01:26 +0300

    no message

--- UIKITDZ/UIKITDZ/StopwatchViewController.swift
+++ UIKITDZ/UIKITDZ/StopwatchViewController.swift
@@ -10,25 +10,28 @@ import UIKit
 /// Экран секундомера
 final class StopwatchViewController: UIViewController {
     
+    // MARK: - Constants
+    private enum Constants {
+        static let countZero = 0
+        static let count = 60
+        static let formatter = "%02d"
+        static let start = "Старт"
+        static let stop = "Стоп"
+        static let reset = "Сброс"
+    }
+    
     // MARK: - Private Outlets
     
     @IBOutlet weak var startBackground: UIView!
     @IBOutlet weak var resetBackground: UIView!
-    @IBOutlet weak var stopBackground: UIView!
-    @IBOutlet weak var lapBackground: UIView!
-    
-    @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var startButton: UIButton!
-    
-    @IBOutlet weak var lapButton: UIButton!
-    
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var watchLabel: UILabel!
     // MARK: - Private properties
-    
-    private let hours = Array(0...23)
-    private let minutes = Array(0...59)
-    private let seconds = Array(0...59)
-    private var timer: Timer?
+     
+    private var timer = Timer()
+    private var tapped = false
+    private var count = Constants.countZero
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -36,70 +39,54 @@ final class StopwatchViewController: UIViewController {
         setupBorders()
     }
     
-    // MARK: - Private Action
-    @IBAction func startButtonAction(_ sender: Any) {
-        startButton.isHidden = true
-        resetButton.isHidden = true
-        
+    // MARK: - Private IBAction
+    @IBAction private func startStopAction(_ sender: Any) {
+        tapped.toggle()
+        guard tapped == true else {
+            startStopButton.setTitle(Constants.start, for: .normal)
+            timer.invalidate()
+            return
+        }
+        startStopButton.setTitle(Constants.stop, for: .normal)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,
+                                     selector: #selector(updateValueAction),
+                                     userInfo: nil, repeats: true)
     }
     
-    @IBAction func resetButtonAction(_ sender: Any) {
+    @IBAction private func resetAction(_ sender: UIButton) {
+        watchLabel.text = makeTimeString(Constants.countZero, Constants.countZero, Constants.countZero)
+        count = Constants.countZero
+        startStopButton.setTitle(Constants.start, for: .normal)
+        tapped = false
+        timer.invalidate()
     }
     
-    @IBAction func stopButtonAction(_ sender: Any) {
-        stopButton.isHidden = true
-        startButton.isHidden = false
+    @objc private func updateValueAction() {
+        count += 1
+        let time = convert(count)
+        let timeString = makeTimeString(time.0, time.1, time.2)
+        watchLabel.text = timeString
     }
     
-    @IBAction func lapButtonAction(_ sender: Any) {
+    // MARK: - Private Properties
+    private func convert(_ seconds: Int) -> (Int, Int, Int) {
+        return ((seconds / Constants.count * Constants.count),
+                ((seconds % (Constants.count * Constants.count)) / Constants.count),
+                ((seconds % (Constants.count * Constants.count)) % Constants.count))
     }
-    //    @IBAction private func startWatchAction(_ sender: Any) {
-//        scheduleTimer()
-//    }
     
-//    @IBAction private func stopWatchAction(_ sender: Any) {
-//        if let minsFirst = self.minutes.first,
-//           let secondsFirst = self.seconds.first,
-//           let hoursFirst = self.hours.first {
-//            self.pickerView.selectRow(hoursFirst, inComponent: 0, animated: true)
-//            self.pickerView.selectRow(minsFirst, inComponent: 1, animated: true)
-//            self.pickerView.selectRow(secondsFirst, inComponent: 2, animated: true)
-//            timer?.invalidate()
-//        }
-//    }
-    
-    // MARK: - Private method
-//    private func scheduleTimer() {
-//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
-//            guard let self = self else { return }
-//            let hour = self.pickerView.selectedRow(inComponent: 0)
-//            let mins = self.pickerView.selectedRow(inComponent: 1)
-//            let secs = self.pickerView.selectedRow(inComponent: 2)
-//            self.pickerView.selectRow(secs + 1, inComponent: 2, animated: true)
-//            switch (hour, mins, secs) {
-//            case (0, 0, 0):
-//                self.timer?.invalidate()
-//            case (0...23, 59, 59):
-//                if let minsFirst = self.minutes.first,
-//                   let secondsFirst = self.seconds.first {
-//                    self.pickerView.selectRow(minsFirst, inComponent: 1, animated: true)
-//                    self.pickerView.selectRow(secondsFirst, inComponent: 2, animated: true)
-//                    self.pickerView.selectRow(hour + 1, inComponent: 0, animated: true)
-//                }
-//            case (0...23, 0...59, 59):
-//                if let secondsFirst = self.seconds.first {
-//                    self.pickerView.selectRow(secondsFirst, inComponent: 2, animated: true)
-//                    self.pickerView.selectRow(mins + 1, inComponent: 1, animated: true)
-//                }
-//            default: break
-//            }
-//        }
-//    }
+    private func makeTimeString(_ hour: Int, _ minutes: Int, _ seconds: Int) -> String {
+        var timeString = ""
+        timeString += String(format: Constants.formatter, hour)
+        timeString += ":"
+        timeString += String(format: Constants.formatter, minutes)
+        timeString += ":"
+        timeString += String(format: Constants.formatter, seconds)
+        return timeString
+    }
     
     private func setupBorders() {
         startBackground.layer.borderColor = UIColor.systemGreen.withAlphaComponent(0.6).cgColor
         resetBackground.layer.borderColor = UIColor.darkGray.withAlphaComponent(0.6).cgColor
-        stopBackground.layer.borderColor = UIColor.systemRed.withAlphaComponent(0.6).cgColor
-        lapBackground.layer.borderColor = UIColor.darkGray.withAlphaComponent(0.6).cgColor
     }
 }
